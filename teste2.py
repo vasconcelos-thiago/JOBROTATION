f102e3cd907366c7fbc45c7a2583ab705ae8e547
INcode <vasconcelosthx@gmail.com>
2023-04-04 00:34:50 -0300

    atividade 2, faturamento diario-atualização

--- teste2.py
+++ teste2.py
@@ -14,7 +14,7 @@ for dia in dados:
     if dia['valor'] < menor_valor:
         menor_valor = dia['valor']
 
-print(f"Menor valor de faturamento: {menor_valor:.2f}")
+print(f"menor valor de faturamento ocorrido em um dia do mês: {menor_valor:.2f}")
 
 #O maior valor de faturamento ocorrido em um dia do mês;
 maior_valor = float('-inf')
@@ -22,16 +22,16 @@ for dia in dados:
     if dia['valor'] > maior_valor:
         maior_valor = dia['valor']
 
-print(f"Maior valor de faturamento: {maior_valor:.2f}")
+print(f"maior valor de faturamento ocorrido em um dia do mês: {maior_valor:.2f}")
 
-# Calculando a média mensal de faturamento
+#aqui vamos calcular a média mensal de faturamento
 soma_valores = 0
 for dia in dados:
     soma_valores += dia['valor']
 
 media_mensal = soma_valores / len(dados)
 
-# Encontrando o número de dias em que o valor de faturamento foi superior à média mensal
+#Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
 dias_acima_media = 0
 for dia in dados:
     if dia['valor'] > media_mensal:
